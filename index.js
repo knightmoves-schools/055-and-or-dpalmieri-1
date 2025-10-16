0ac455359dd045728185de3ed2d7ce23556099de
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-16 19:33:57 +0000

    Rework?

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function taxable(state, taxExempt){
-    if (state == IA && taxExempt == false){
+    if (state == IA && taxExempt === false){
         return 'is taxable';
     }
 }
+
 function isVehicle(hasWheels, canFly, canSwim){
     if (hasWheels == true || canFly == true || canSwim == true){
         return 'is vehicle';
     }
 }
+
+document.getElementById('result').innerHTML = taxable(IA, false);
